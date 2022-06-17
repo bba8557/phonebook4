d3f29d6a0c229f01a2f06f2539251e3dec2709da
bba8557 <bba8557@naver.com>
2022-06-17 16:29:39 +0900

    map

--- src/main/java/com/javaex/dao/PhoneDao.java
+++ src/main/java/com/javaex/dao/PhoneDao.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
 
 import javax.sql.DataSource;
 
@@ -33,9 +34,20 @@ public class PhoneDao {
  
    //사람 추가
    public int personInsert(PersonVo personVo) {
-	   System.out.println("PhoneController>list()");
+	   System.out.println("PhoneDao>personInsert()");
 	   
 	   int count = sqlSession.insert("phonebook.personInsert", personVo);
+	   
+	   return count;
+   }
+   
+   //사람추가(map를 사용하는 가사으이 상황: 문법설명)
+   public int perosnInsert2(Map<String, String> pMap) {
+	   System.out.println("PhoneDao>personInsert2()");
+	   
+	   //System.out.println(pMap.toString());
+	   
+	   int count = sqlSession.insert("phonebook.personInsert2", pMap);
 	   
 	   return count;
    }
@@ -51,13 +63,27 @@ public class PhoneDao {
    
    //1명 정보 가져오기
    public PersonVo getPerson(int no) {
-	   System.out.println("PhoneController>list()");
+	   System.out.println("PhoneDao>getPerson");
 	   
 	   PersonVo personVo = sqlSession.selectOne("phonebook.getPerson", no);
 	   System.out.println(personVo);
 	   
 	   return personVo;
    }
+   
+ //1명 정보 가져오기 map을 사용하는 가상의 상황: 문법설명
+   public Map<String, Object> getPerson2(int no) {
+	   System.out.println("PhoneDao>getPerson2");
+	   System.out.println(no);
+	   
+	   Map<String, Object> pMap = sqlSession.selectOne("phonebook.getPerson2", no);
+	   System.out.println(pMap);
+	   
+	   return pMap;
+   }
+   
+   
+   
    
    //사람 수정
    public int personUpdate(PersonVo personVo) {
--- src/main/java/com/javaex/service/PhoneService.java
+++ src/main/java/com/javaex/service/PhoneService.java
@@ -1,6 +1,8 @@
 package com.javaex.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,11 +42,25 @@ public class PhoneService {
 		return count;
 	}
 	
+	//전화번호 등록(map를 사용하는 가사으이 상황: 문법설명)
+	public int personInsert2() {
+		System.out.println("PhoneController>list()");
+		
+		//map을 만들어 사용한다* 1회성
+		Map<String, String> pMap = new HashMap<String, String>();
+		pMap.put("name", "황일영");
+		pMap.put("hp", "010-3333-3333");
+		pMap.put("company", "02-3333-3333");
+		
+		int count = phoneDao.perosnInsert2(pMap);
+		
+		return count;
 	
+	}
 	
 	//전화번호 삭제
 	public int personDelete(int no) {
-		System.out.println("PhoneController>list()");
+		System.out.println("PhoneService>getPerson()");
 		
 		int count = phoneDao.personDelete(no);
 		
@@ -53,18 +69,32 @@ public class PhoneService {
 	
 	//전화번호 수정폼(정보가져오기)
 	public PersonVo getPerson(int no) {
-		System.out.println("PhoneController>list()");
+		System.out.println("PhoneService>getPerson()");
 		
 		PersonVo personVo = phoneDao.getPerson(no);
+		
+		//Map 사용 연습
+		//PersonVo personVo = phoneDao.getPerson2(no);
 		
 		return personVo;
 	}
 	
+	//전화번호 수정폼2(정보가져오기)
+	public Map<String, Object> getPerson2(int no) {
+		System.out.println("PhoneService>getPerson2()");
+		
+		Map<String, Object> pMap = phoneDao.getPerson2(no);
+		
+		return pMap;
+	}
+	
+	
 	//전화번호 수정
 	public int personUpdate(PersonVo personVo) {
-		System.out.println("PhoneController>list()");
+		System.out.println("PhoneService>personUpdate()");
 		
 		int count = phoneDao.personUpdate(personVo);
+		
 		
 		return count;
 	}
